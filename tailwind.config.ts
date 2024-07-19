7300cfa7baca87ffb5c9537781e463023a5bc960
Ronak Ganatra <20298819+ronakganatra@users.noreply.github.com>
2024-07-19 09:24:44 +0200

    chore: font casing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        geist: ['Geist', 'sans-serif'],
-        epilogue: ['Epilogue', 'sans-serif'],
-        outfit: ['Outfit', 'sans-serif'],
+        geist: ['geist', 'sans-serif'],
+        epilogue: ['epilogue', 'sans-serif'],
+        outfit: ['outfit', 'sans-serif'],
       },
       colors: {
         neutral: {
