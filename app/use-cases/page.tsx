871f6b7b880d6008a2f73bc34a295d51e3fade57
Ronak Ganatra <20298819+ronakganatra@users.noreply.github.com>
2024-08-04 12:44:02 +0200

    chore: clean up indexes
    Swap ContentBlock for CardGrid on Index Pages

--- app/use-cases/page.tsx
+++ app/use-cases/page.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import Nav from '../../components/Nav';
 import Hero from '../../components/Hero';
-import ContentBlock from '@/components/ContentBlock';
+import CardGrid from '../../components/CardGrid';
+import Cta from '@/components/Cta';
 import Footer from '@/components/Footer';
 
 
 const UseCasesPage: React.FC = () => {
+
+
+  const UseCaseCards = [
+    {
+      imageUrl: 'https://res.cloudinary.com/savepad-web/image/upload/v1715850633/Landing%20Pages/Personas/Growth_Teams_cyw32z.png',
+      imageAlt: 'Manage',
+      title: 'Swipe File',
+      description: "Save, reference, and curate your resources with your team.",
+      link: '/use-cases/swipe-file',
+    },
+    {
+      imageUrl: 'https://res.cloudinary.com/savepad-web/image/upload/v1715850632/Landing%20Pages/Personas/Creatives_and_Agencies_axpunw.png',
+      imageAlt: 'Collaborate',
+      title: 'Knowledge Base',
+      description: "Collect everything in a simple, searchable, and collaborative knowledge base.",
+      link: '/use-cases/knowledge-base',
+    },
+    {
+      imageUrl: 'https://res.cloudinary.com/savepad-web/image/upload/v1715850632/Landing%20Pages/Personas/Creatives_and_Agencies_axpunw.png',
+      imageAlt: 'Find',
+      title: 'Bookmark Manager',
+      description: "Travel plans, meal ideas, whatever. Save them for you, or to share with friends and family.",
+      link: '/use-cases/bookmark-manager',
+    },
+  ];
+
+
   return (
     <main>
       <Nav />
@@ -20,41 +48,18 @@ const UseCasesPage: React.FC = () => {
         buttonVariant="primary"
       />
 
+<CardGrid cards={UseCaseCards} columns={2} />
 
-      <ContentBlock
-        imageUrl="https://res.cloudinary.com/savepad-web/image/upload/v1715850633/Landing%20Pages/Personas/Growth_Teams_cyw32z.png"
-        imageAlt="For Growth Teams"
-        heading="Swipe File"
+<Cta
+        title="Get started for free"
         paragraphs={[
-          "Ever wanted to kick-off a new landing page and can’t find that one amazing example from 3 years ago that had the words “pay as you grow”?",
-          "Only felt more frustrated when bringing in your team to collaborate?"
+          "Savepad's generous free tier gives you LIMITS.",
+          "Our first 250 waitlist users will get lifetime access to Savepad for free."
         ]}
-        buttonLink='/use-cases/swipe-file'
-        buttonText='Learn More'
+        buttonText="Sign Up for Free"
+        buttonHref="/signup"
       />
-      <ContentBlock
-        imageUrl="https://res.cloudinary.com/savepad-web/image/upload/v1715850632/Landing%20Pages/Personas/Creatives_and_Agencies_axpunw.png"
-        imageAlt="Creatives and Devs"
-        heading="Knowledge Base"
-        paragraphs={[
-          "URLs, screenshots, emails, files, and heaven knows what else stuck in limbo between sheets, Notions, Slacks, emails, and other tools?",
-          "Collect everything you’ve ever found in a simple, searchable, and collaborative knowledge base."
-        ]}
-        buttonLink='/use-cases/knowledge-base'
-        buttonText='Learn More'
-        reverse
-      />
-            <ContentBlock
-        imageUrl="https://res.cloudinary.com/savepad-web/image/upload/v1715850632/Landing%20Pages/Personas/Creatives_and_Agencies_axpunw.png"
-        imageAlt="Creatives and Devs"
-        heading="Bookmark Manager"
-        paragraphs={[
-          "It's the 2020s. Why is it still so damn hard to manage bookmarks?",
-          "Travel plans, meal ideas, home inspo, new boots, whatever tickles your fancy has a home here. Just for you, or to share with friends and family."
-        ]}
-        buttonLink='/use-cases/bookmark-manager'
-        buttonText='Learn More'
-      />
+      
 
 <Footer />
     </main>
